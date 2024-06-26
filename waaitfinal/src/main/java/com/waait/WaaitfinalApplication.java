ee4f8c8a8785ef04810360a05493021276493e56
GDJ <GDJ@DESKTOP-ECFFCGV>
2024-06-26 10:39:39 +0900

    final 프로젝트

---
+++ waaitfinal/src/main/java/com/waait/WaaitfinalApplication.java
@@ -0,0 +1,13 @@
+package com.waait;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaaitfinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaaitfinalApplication.class, args);
+	}
+
+}
---
+++ waaitfinal/src/test/java/com/waait/WaaitfinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waait;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaaitfinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
