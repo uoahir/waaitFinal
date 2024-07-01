3d4f91ec3f72b0a3a1d1b72b12b93af167165b6b
leeae <leeae@host.docker.internal>
2024-07-01 15:32:44 +0900

    session 까지함 -yh

---
+++ waaitfinal/src/main/java/com/waait/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.waait.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.waait.common.EmpAuthority;
+import com.waait.security.controller.UserPasswordAuthenticationProvider;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	// 대칭키
+	
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http, UserPasswordAuthenticationProvider provider)
+			throws Exception {
+		http.csrf(csrf -> csrf.disable())
+				.authorizeHttpRequests(authorizeRequests -> authorizeRequests
+						.requestMatchers("/logininfo", "/user","/login", "/WEB-INF/views/**", "/resources/**").permitAll()
+						.requestMatchers("/admin").hasAnyAuthority(EmpAuthority.ADMIN.name())
+						.anyRequest().authenticated()
+						)
+				.formLogin(formLogin -> formLogin.loginPage("/login")
+						.loginProcessingUrl("/logininfo")
+						.defaultSuccessUrl("/user")
+						.usernameParameter("user")
+						.passwordParameter("password"))
+				.authenticationProvider(provider)
+				.logout(logout -> logout.logoutSuccessUrl("/login").invalidateHttpSession(true)
+				).sessionManagement(session->
+				session.maximumSessions(1)
+				)
+				;
+		return http.build();
+	}
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
---
+++ waaitfinal/src/main/java/com/waait/controller/HomeController.java
@@ -0,0 +1,61 @@
+package com.waait.controller;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.waait.dto.Employee;
+import com.waait.service.EmployeeService;
+
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class HomeController {
+
+	private final EmployeeService employeeService;
+	private final BCryptPasswordEncoder encoder;
+
+	@GetMapping("/login")
+	public String loginForm() {
+		System.out.println("/loginPage");
+
+		return "login";
+	}
+
+	@GetMapping("/user")
+	public String homeForm() {
+		System.out.println("/userPage");
+		return "home";
+
+	}
+
+	@GetMapping("/")
+	public String homeForm1() {
+		System.out.println("/userPage");
+		return "redirect:/user";
+
+	}
+
+	@PostMapping("/insertemp")
+	public String testInsertEmp(Employee e) {
+		System.out.println("이름:" + e.getEmpName());
+		System.out.println("아이디:" + e.getEmpId());
+		System.out.println("비밀번호:" + e.getEmpPwd());
+		Employee employee = e.builder().empId(e.getEmpId()).empName(e.getEmpName())
+				.empPwd(encoder.encode(e.getEmpPwd())).build();
+		employeeService.insertEmployee(employee);
+		return "redirect:/user";
+	}
+
+	@GetMapping("/codeboard")
+	public String codePage(HttpSession session) {
+		Employee employee= (Employee)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		System.out.println("session"+employee);
+		return "codereviewboard/codeboard";
+	}
+
+}
