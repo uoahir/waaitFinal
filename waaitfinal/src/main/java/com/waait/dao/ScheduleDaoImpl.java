fb1bb3e32f365108f2f32e843982e00665c32e34
youjh <youjh@LAPTOP-REVK6GLB>
2024-07-08 17:53:14 +0900

    <jh> Schedule 만드는 중

--- waaitfinal/src/main/java/com/waait/dao/ScheduleDaoImpl.java
+++ waaitfinal/src/main/java/com/waait/dao/ScheduleDaoImpl.java
@@ -1,11 +1,25 @@
 package com.waait.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
+
+import com.waait.dto.Schedule;
 
 import lombok.RequiredArgsConstructor;
 
 @Repository
 @RequiredArgsConstructor
 public class ScheduleDaoImpl implements ScheduleDao {
+	
+	private final SqlSession session;
+	
+	@Override
+	public List<Schedule> selectByempNo(SqlSession session,Schedule s) {
+		return session.selectList("schedule.selectByempNo", s);
+	}
+	
+	
 
 }
