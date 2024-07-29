32da77f3d967686031fa4f97ffac80eee530b357
youjh <youjh@LAPTOP-REVK6GLB>
2024-07-29 09:39:31 +0900

    <jh>
    Merge branch 'testwaait' of https://github.com/K-SOLMIN/waaitFinal.git
    into waait

--- waaitfinal/src/main/java/com/waait/dto/Mypage.java
+++ waaitfinal/src/main/java/com/waait/dto/Mypage.java
@@ -20,15 +20,5 @@ public class Mypage {
 	private int vacationNo; // 휴가 고유번호
 	private int workNo; // 근태현황 고유변호 
 	
-	//이렇게 하면 session에 저장된 사원 정보 가져올 수 있음 
-//	request.getSession() 이렇게 했던거랑 같음
-//	public void test() {
-//		Employee employee = (Employee) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//	}
-
 }
 
-/*
- * @Pattern(regexp="(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*()]).{8,}" //비밀번호 정규 표현식
- * ,message="비밀번호는 소문자,숫자,특수기호를 포함한 8글자 이상으로 작성하세요")
- */
